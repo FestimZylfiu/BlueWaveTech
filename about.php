f62dac34f5940e981b4484c7574dde8b2ab82c2a
FestimZylfiu <fz57195@ubt-uni.net>
2024-06-13 00:12:14 +0200

    added about page

---
+++ about.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="style2.css">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"/>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <a href="#" class="logo">BlueWaveTech</a>
+        <i class='bx bx-menu' id="menu-icon"></i>
+        <nav class="navbar">
+            <a href="index.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="product.php">Product</a>
+            <a href="contact.php">Contact</a>
+            <a href="login.php">Login</a>
+        </nav>
+    </header>
+    <div class="nav-bg"></div>
+
+    <div class="content-container">
+        <div class="text-container" data-aos="fade-right">
+            <h1>About us</h1>
+            <p>Welcome to BlueWaveTech, your one-stop destination for all things technology! 
+                We offer a wide range of top-notch products including the latest smartphones, high-definition TVs, cutting-edge laptops, and must-have gadgets. 
+                At Tech Haven, we pride ourselves on providing our customers with the newest innovations and the best deals on the market. 
+                Whether you're a tech enthusiast or simply looking for reliable devices to enhance your daily life, we've got you covered. 
+                Explore our collection today and experience the future of technology with Tech Haven!</p>
+        </div>
+        <div class="image-container" data-aos="fade-left">
+            <img src="images/tech2.png" alt="Sample Image">
+        </div>
+    </div>
+
+    <div class="background-div">
+        <div>
+            <span class="text">&nbsp;upcoming...&nbsp;</span>
+      <span class="hover-text">&nbsp;upcoming...&nbsp;</span>
+        </div>
+        <video class="video" width="500" height="300" autoplay loop muted>
+            <source src="iPhone 15.mp4" type="video/mp4">
+        </video>
+    </div>
+
+    <h3 class="team-title">OUR TEAM</h3>
+    <section>
+        <div class="swiper mySwiper container">
+          <div class="swiper-wrapper content">
+    
+            <div class="swiper-slide card">
+              <div class="card-content">
+                <div class="image">
+                  <img src="img/img1.jpg" alt="">
+                </div>
+    
+                <div class="social-media">
+                  <i class="fa-brands fa-facebook"></i>
+                  <i class="fa-brands fa-twitter"></i>
+                  <i class="fa-brands fa-github"></i>
+                </div>
+    
+                <div class="name-profession">
+                  <span class="name">Liam Johnson</span>
+                  <span class="profession">Web Developer</span>
+                </div>
+    
+                <div class="rating">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+    
+                <div class="button">
+                  <button class="aboutMe">About Me</button>
+                  <button class="hireMe">Hire Me</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide card">
+              <div class="card-content">
+                <div class="image">
+                  <img src="img/img2.jpg" alt="">
+                </div>
+    
+                <div class="social-media">
+                  <i class="fa-brands fa-facebook"></i>
+                  <i class="fa-brands fa-twitter"></i>
+                  <i class="fa-brands fa-github"></i>
+                </div>
+    
+                <div class="name-profession">
+                  <span class="name">Olivia Thompson</span>
+                  <span class="profession">Front End Developer</span>
+                </div>
+    
+                <div class="rating">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+    
+                <div class="button">
+                  <button class="aboutMe">About Me</button>
+                  <button class="hireMe">Hire Me</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide card">
+              <div class="card-content">
+                <div class="image">
+                  <img src="img/img3.jpg" alt="">
+                </div>
+    
+                <div class="social-media">
+                  <i class="fa-brands fa-facebook"></i>
+                  <i class="fa-brands fa-twitter"></i>
+                  <i class="fa-brands fa-github"></i>
+                </div>
+    
+                <div class="name-profession">
+                  <span class="name">Noah Smith</span>
+                  <span class="profession">Back End Developer</span>
+                </div>
+    
+                <div class="rating">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+    
+                <div class="button">
+                  <button class="aboutMe">About Me</button>
+                  <button class="hireMe">Hire Me</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide card">
+              <div class="card-content">
+                <div class="image">
+                  <img src="img/img4.jpg" alt="">
+                </div>
+    
+                <div class="social-media">
+                  <i class="fa-brands fa-facebook"></i>
+                  <i class="fa-brands fa-twitter"></i>
+                  <i class="fa-brands fa-github"></i>
+                </div>
+    
+                <div class="name-profession">
+                  <span class="name">Ethan Williams</span>
+                  <span class="profession">Web Developer</span>
+                </div>
+    
+                <div class="rating">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+    
+                <div class="button">
+                  <button class="aboutMe">About Me</button>
+                  <button class="hireMe">Hire Me</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide card">
+              <div class="card-content">
+                <div class="image">
+                  <img src="img/img5.jpg" alt="">
+                </div>
+    
+                <div class="social-media">
+                  <i class="fa-brands fa-facebook"></i>
+                  <i class="fa-brands fa-twitter"></i>
+                  <i class="fa-brands fa-github"></i>
+                </div>
+    
+                <div class="name-profession">
+                  <span class="name">Isabella Carter</span>
+                  <span class="profession">Web Developer</span>
+                </div>
+    
+                <div class="rating">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+    
+                <div class="button">
+                  <button class="aboutMe">About Me</button>
+                  <button class="hireMe">Hire Me</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide card">
+              <div class="card-content">
+                <div class="image">
+                  <img src="img/img6.jpg" alt="">
+                </div>
+    
+                <div class="social-media">
+                  <i class="fa-brands fa-facebook"></i>
+                  <i class="fa-brands fa-twitter"></i>
+                  <i class="fa-brands fa-github"></i>
+                </div>
+    
+                <div class="name-profession">
+                  <span class="name">Mason Brown</span>
+                  <span class="profession">Back End Developer</span>
+                </div>
+    
+                <div class="rating">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+    
+                <div class="button">
+                  <button class="aboutMe">About Me</button>
+                  <button class="hireMe">Hire Me</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide card">
+              <div class="card-content">
+                <div class="image">
+                  <img src="img/img7.jpg" alt="">
+                </div>
+    
+                <div class="social-media">
+                  <i class="fa-brands fa-facebook"></i>
+                  <i class="fa-brands fa-twitter"></i>
+                  <i class="fa-brands fa-github"></i>
+                </div>
+    
+                <div class="name-profession">
+                  <span class="name">Alexander Miller</span>
+                  <span class="profession">Web Developer</span>
+                </div>
+    
+                <div class="rating">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+    
+                <div class="button">
+                  <button class="aboutMe">About Me</button>
+                  <button class="hireMe">Hire Me</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide card">
+              <div class="card-content">
+                <div class="image">
+                  <img src="img/img8.jpg" alt="">
+                </div>
+    
+                <div class="social-media">
+                  <i class="fa-brands fa-facebook"></i>
+                  <i class="fa-brands fa-twitter"></i>
+                  <i class="fa-brands fa-github"></i>
+                </div>
+    
+                <div class="name-profession">
+                  <span class="name">Emma Anderson</span>
+                  <span class="profession">Back End Developer</span>
+                </div>
+    
+                <div class="rating">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+    
+                <div class="button">
+                  <button class="aboutMe">About Me</button>
+                  <button class="hireMe">Hire Me</button>
+                </div>
+              </div>
+            </div>
+            <div class="swiper-slide card">
+              <div class="card-content">
+                <div class="image">
+                  <img src="img/img9.jpg" alt="">
+                </div>
+    
+                <div class="social-media">
+                  <i class="fa-brands fa-facebook"></i>
+                  <i class="fa-brands fa-twitter"></i>
+                  <i class="fa-brands fa-github"></i>
+                </div>
+    
+                <div class="name-profession">
+                  <span class="name">Benjamin Wilson</span>
+                  <span class="profession">Front End Developer</span>
+                </div>
+    
+                <div class="rating">
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-solid fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                  <i class="fa-regular fa-star"></i>
+                </div>
+    
+                <div class="button">
+                  <button class="aboutMe">About Me</button>
+                  <button class="hireMe">Hire Me</button>
+                </div>
+              </div>
+            </div>
+    
+          </div>
+        </div>
+    
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+      </section>
+
+      <?php
+include "pixijs2.php";
+?>
+
+
+
+
+    <br>
+    <br>
+
+      
+
+      <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+
+<script type="text/javascript" src="script.js"></script>
+<script type="text/javascript" src="script2.js"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+  </script>
+
+</body>
+</html>
